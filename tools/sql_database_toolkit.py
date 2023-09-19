6fc041d1b0ffcca24368a486fa107ba03e90f41b
Fay Elhassan <38003091+FayElhassan@users.noreply.github.com>
2023-09-19 10:25:54 +0000

    Update sql_database_toolkit.py

--- tools/sql_database_toolkit.py
+++ tools/sql_database_toolkit.py
@@ -1,13 +1,20 @@
+
 # tools/sql_database_toolkit.py
 
 from langchain.sql_database import SQLDatabase
-from sqlalchemy import create_engine
 from config.settings import POSTGRESQL, PostgresConfig
-# from sql_analyzer.log_init import logger  # Uncomment if you want to use the logger from sql_analyzer
 
 def sql_db_factory() -> SQLDatabase:
     if POSTGRESQL:
-        db = SQLDatabase.from_uri(f"postgresql+psycopg2://{PostgresConfig.user}:{PostgresConfig.password}@{PostgresConfig.host}:{PostgresConfig.port}/{PostgresConfig.database}")
+        db_uri = (
+            f"postgresql+psycopg2://"
+            f"{PostgresConfig.user}:"
+            f"{PostgresConfig.password}@"
+            f"{PostgresConfig.host}:"
+            f"{PostgresConfig.port}/"
+            f"{PostgresConfig.database}"
+        )
+        db = SQLDatabase.from_uri(db_uri)
         return db
     else:
-        raise Exception(f"Could not create sql database factory.")
+        raise Exception(f"POSTGRESQL setting was set to False or not provided.")
